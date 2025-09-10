dad9032a792350b280ac8e55755b1ea31394c280
ZerekH <zerekhenderson@gmail.com>
2025-09-10 14:34:19 -0400

    adds drive_square function

--- lab1.py
+++ lab1.py
@@ -1,16 +1,19 @@
 from mbot_bridge.api import MBot
 import time
+def drive_square(squares:int):
+    my_robot = MBot()  # intialize the robot
 
-my_robot = MBot()  # intialize the robot
+    for i in range(squares):
+        for j in range(4):
+            # move for half a second
+            my_robot.drive(1, 0, 0) 
+            time.sleep(.5)
+            # stop
+            my_robot.stop()
+            # turn 90 degrees(1.5708 radians) for 1 second
+            my_robot.drive(0, 0, 1.5708)
+            time.sleep(1)
+            # stoping the rotation
+            my_robot.stop()
 
-# move for half a second
-my_robot.drive(1, 0, 0) 
-time.sleep(.5)
-# stop
-my_robot.stop()
-# turn 90 degrees(1.5708 radians) for 1 second
-my_robot.drive(0, 0, 1.5708)
-time.sleep(1)
-# stoping the rotation
-my_robot.stop()
-
+drive_square(3)
